3d6c62888b239890fa88d708ff2c4f8cd25f0e84
e <jannatul0040@gmail.com>
2023-11-05 21:12:13 +0600

    created basic setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import { Button } from 'flowbite-react'
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className='max-w-7xl mx-auto'>
+      <h1 className='text-2xl font-medium'>Vite + React</h1>
+      <Button className='' color="blue">Blueee</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
