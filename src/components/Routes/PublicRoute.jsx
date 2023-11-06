4277cf1a57aea088208033b6d1863b95eb788b67
e <jannatul0040@gmail.com>
2023-11-06 10:40:56 +0600

    created folder structure

---
+++ src/components/Routes/PublicRoute.jsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import ErrorPage from "../Error/ErrorPage";
+import Home from "../Home/Home";
+import Services from "../Services/Services";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout></MainLayout>,
+      errorElement:<ErrorPage></ErrorPage>,
+      children:[
+        {
+            path:'/',
+            element:<Home></Home>
+        },
+        {
+            path:'/services',
+            element: <Services></Services>
+        }
+      ]
+    },
+    {
+        path:'/login',
+        element: <Login></Login>
+    },
+    {
+        path:'/register',
+        element: <Register></Register>
+    }
+  ]);
+
+
+export default router;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import router from './components/Routes/PublicRoute.jsx';
+import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <div className="max-w-7xl mx-auto">
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
+  </React.StrictMode>
+  </div>
 )
