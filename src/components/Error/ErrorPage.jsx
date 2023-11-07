e10ddd32be17b93e3cafb586f961e667900a1416
e <jannatul0040@gmail.com>
2023-11-08 01:11:26 +0600

    updated error page and used lottie animation

--- src/components/Error/ErrorPage.jsx
+++ src/components/Error/ErrorPage.jsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom';
-import errorpic from '../../assets/images/error.jpg'
+import Lottie from 'lottie-react';
+import error from '../../assets/errorAnimation.json'
 
 const ErrorPage = () => {
     return (
         <div className='max-w-7xl mx-auto'>
-            <img className='h-1/3 w-1/3 mx-auto' src={errorpic} alt="" />
-            <h1 className='text-center'>404-PAGE NOT FOUND</h1>
+            <Lottie className='h-1/3 w-1/3 mx-auto' animationData={error}></Lottie>
+            <h1 className='text-center font-bold'>404-PAGE NOT FOUND</h1>
             <p className='py-5 text-center'>The page you are looking for might have been removed <br></br>had its name changed or is temporary unavailable</p>
-            <button className='btn btn-primary flex justify-center items-center mx-auto'><Link to='/'>Go Home</Link></button>
+            <button className='btn btn-outline text-[#8e4afc] btn-sm hover:bg-[#8e4afc] hover:border-none hover:text-white flex justify-center items-center mx-auto'><Link to='/'>Go Home</Link></button>
         </div>
     );
 };
