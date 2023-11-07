929979149508890ff3502b9908f981c6ee73d7ac
e <jannatul0040@gmail.com>
2023-11-08 02:15:40 +0600

    added login and register page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,37 +1,46 @@
-
+import Lottie from "lottie-react";
+import login from '../../assets/login.json'
+import { Link } from "react-router-dom";
 
 const Login = () => {
     return (
-        <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-    </div>
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <form className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+
+        <div>
+            <h1 className="text-center text-4xl font-bold pt-10"><span className="text-[#8e4afc]">Login</span> Now</h1>
+            <div>
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                        <Lottie animationData={login}></Lottie>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-outline text-[#8e4afc] hover:bg-[#8e4afc] hover:border-none hover:text-white">Login</button>
+                            </div>
+                        </form>
+                        <div>
+                            <p className="px-8">New to this site? Please <Link className="text-[#8e4afc] font-medium" to='/register'>Register</Link></p>
+                            <p className="text-center">or</p>
+                            <div className="form-control mt-6 px-8 pb-8">
+                            <button className="btn btn-outline text-[#8e4afc] hover:bg-[#8e4afc] hover:border-none hover:text-white">Sign in With Google</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
     );
 };
 
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,10 +1,55 @@
-
+import Lottie from "lottie-react";
+import register from '../../assets/register.json'
+import { Link } from "react-router-dom";
 
 const Register = () => {
     return (
         <div>
-            register
+        <h1 className="text-center text-4xl font-bold pt-10"><span className="text-[#8e4afc]">Register</span> Now</h1>
+        <div>
+
+            <div className="hero-content flex-col lg:flex-row-reverse">
+
+                <div className="text-center lg:text-left">
+                    <Lottie className="h-1/2 w-1/2 mx-auto" animationData={register}></Lottie>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="name" placeholder="Enter your name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo URL</span>
+                            </label>
+                            <input type="url" placeholder="Photo URL" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="Enter your email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-outline text-[#8e4afc] hover:bg-[#8e4afc] hover:border-none hover:text-white">Register</button>
+                        </div>
+                    </form>
+                    <div>
+                    <p className="px-8 pb-8">Already have an account? Please <Link className="text-[#8e4afc] font-medium" to='/login'>Login</Link></p>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
     );
 };
 
